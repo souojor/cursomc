a083f622a9d2dcaf22befb8c5964aea752f37d7e
Jorlano Donizete Silveira Pereira <souojor@gmail.com>
2019-06-22 18:51:33 -0300

    Endpoint /pedidos/{id} disponibilizado.

--- src/main/java/br/com/souojor/cursormc/domain/Pedido.java
+++ src/main/java/br/com/souojor/cursormc/domain/Pedido.java
@@ -1,10 +1,8 @@
 package br.com.souojor.cursormc.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -17,6 +15,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -24,11 +25,15 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 
+	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
@@ -48,14 +53,6 @@ public class Pedido implements Serializable {
 		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
-	}
-
-	public List<Pedido> getPedidos() {
-		List<Pedido> lista = new ArrayList<>();
-		for (ItemPedido itemPedido : itens) {
-			lista.add(itemPedido.getPedido());
-		}
-		return lista;
 	}
 
 	public Integer getId() {
---
+++ src/main/java/br/com/souojor/cursormc/services/PedidoService.java
@@ -0,0 +1,21 @@
+package br.com.souojor.cursormc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.souojor.cursormc.domain.Pedido;
+import br.com.souojor.cursormc.repositories.PedidoRepository;
+import br.com.souojor.cursormc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repository.findById(id)
+				.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+		return obj;
+	}
+}
